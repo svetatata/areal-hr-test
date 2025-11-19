379dfc2a7bd6ce34a123c2eae401347f2d622e7f
svetatata <svetlana.ts.05@mail.ru>
2025-11-19 10:41:00 +0300

    add sotrudniki filtering

--- app.js
+++ app.js
@@ -5,20 +5,26 @@ const app = express()
 const port = 8080
 app.set("view engine", "ejs")
 app.use(express.static("views"))
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', async (req, res) => {
-    const sotrudniki = await getSotrudniki()
+    const thisOtdel = req.query.otdel || ''
+    const thisPos = req.query.position || ''
+    const sotrudniki = await getSotrudniki(thisOtdel, thisPos)
     const otdels = await getOtdels()
     const positions = await getPos()
     res.render("index.ejs", {
-    sotrudniki,
-    otdels, 
-    positions,
-    thisOtdel: req.query.otdel || '', 
-    thisPos: req.query.position || ''
+        sotrudniki,
+        otdels,
+        positions,
+        thisOtdel,
+        thisPos
     })
 })
-
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('ломай ломай мы же миллионеры')
+})
 app.listen(port, () => {
-    console.log('порт http://localhost:' + port)
+    console.log('корабль прибыл в порт http://localhost:' + port)
 })
--- database.js
+++ database.js
@@ -7,8 +7,23 @@ const pool = mysql.createPool({
   charset: 'utf8mb4'
 }).promise()
 
-export async function getSotrudniki() {
-    let query = "select * from sotrudniki";
+export async function getSotrudniki(otdel = '', position = '') {
+    let query = "select s.*, o.name as o_name, p.name as p_name from sotrudniki s join otdels o on s.otdel_id = o.id join positions p on s.position_id = p.id";
+    let condit = [];
+    let params = [];
+    
+    if (otdel != '') {
+        condit.push('o.name = ?');
+        params.push(otdel);
+    } 
+    if (position != '') {
+        condit.push('p.name = ?');
+        params.push(position);
+    }
+    if (condit.length > 0) {
+        query += ' where ' + condit.join(' and ');
+    }
+    query += ' order by s.last_name';
     const [rows] = await pool.query(query)
     return rows
 }
